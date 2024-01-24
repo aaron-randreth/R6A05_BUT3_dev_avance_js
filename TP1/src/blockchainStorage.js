d72f51c06b7121b1f740730bd90c599b27c8fa92
Aaron RANDRETH <aaron.randreth@protonmail.ch>
2024-01-24 11:29:34 +0100

    Refactor: better use of await to be more async

--- TP1/src/blockchainStorage.js
+++ TP1/src/blockchainStorage.js
@@ -79,16 +79,11 @@ export async function verifyBlocks() {
     return res
 }
 
-/**
- * Creation d'un block depuis le contenu json
- * @param contenu
- * @return {Promise<Block[]>}
- */
-export async function createBlock(contenu) {
+async function init_new_block(name, donation){
    let newBlock = { 
        id: uuidv4(), 
-       nom: contenu.nom, 
-       don: contenu.don, 
+       nom: name,
+       don: donation,
        date: getDate(),
     }
 
@@ -97,10 +92,19 @@ export async function createBlock(contenu) {
     if (last_block != null)
         newBlock.hash = hash(last_block)
 
-    const old_block_ledger = await findBlocks()
+    return newBlock
+}
 
-    const new_ledger = [...old_block_ledger, newBlock]
+/**
+ * Creation d'un block depuis le contenu json
+ * @param contenu
+ * @return {Promise<Block[]>}
+ */
+export async function createBlock(contenu) {
+    const [old_ledger, new_block] = await Promise.all([findBlocks(), init_new_block(contenu.nom, contenu.don)])
+    const new_ledger = [...old_ledger, new_block]
 
+    // Wait until we are sure the block was added
     await writeFile(path, JSON.stringify(new_ledger), {encoding:'utf-8'})
 
     return new_ledger
