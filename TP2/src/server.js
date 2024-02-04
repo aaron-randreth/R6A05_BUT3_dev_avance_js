77db1f822e4956ed6a5272b69d9f15aa75ee8ab7
Aaron RANDRETH <aaron.randreth@protonmail.ch>
2024-02-04 15:51:08 +0100

    Feat: TP2, étape 4, affichage des personnages.
    - Utilisation de handlebars pour l'affichage
    - Utilisation des partials .hbs
    - Affichage des persos avec les cards bootstrap

--- TP2/src/server.js
+++ TP2/src/server.js
@@ -1,11 +1,29 @@
 import {getData} from './api.js'
+
 import Fastify from 'fastify'
+
+import fastify_view from '@fastify/view'
+import handlebars from 'handlebars'
+
 const app = Fastify()
 
+app.register(fastify_view, {
+  engine: {
+    handlebars : handlebars
+  },
+  options: {
+    partials: {
+      header: '/views/partials/header.hbs',
+      footer: '/views/partials/footer.hbs'
+    }
+  } 
+})
+
 app.get('/', async (req, res) => {
-  res.headers({'Content-Type': 'application/json'})
-  res.code(200)
-  res.send(await getData("http://gateway.marvel.com" + "/v1/public/characters"))
+  //res.headers({'Content-Type': 'application/json'})
+  //res.code(200)
+  //res.send())
+  return res.view('/views/templates/index.hbs', {personnes: await getData("http://gateway.marvel.com" + "/v1/public/characters")})
 })
 
 app.listen({ port: 3000 })
